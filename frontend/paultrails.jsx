91ff0047ce4f74d56a11bf333476919029667980
Paul Kim <paulwkim30@gmail.com>
2021-07-04 12:34:25 -0700

    add session api

--- frontend/paultrails.jsx
+++ frontend/paultrails.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { signUp, login, logout } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
+
+  
+  
   ReactDOM.render(<h2>Welcome to PaulTrails!</h2>, root)
 });
+
+
+
+
+// window.signUp = signUp;
+// window.login = login;
+// window.logout = logout;
