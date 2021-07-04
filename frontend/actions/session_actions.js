edf6856398c2c05af928a482f6a1068a4c46003e
Paul Kim <paulwkim30@gmail.com>
2021-07-04 14:03:46 -0700

    add session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,46 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const CLEAR_SESSION_ERRORS = "CLEAR_SESSION_ERRORS";
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+const clearSessionErrors = () => ({
+  type: CLEAR_SESSION_ERRORS
+});
+
+export const login = user => dispatch => (
+  SessionAPIUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const logout = () => dispatch => (
+  SessionAPIUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()))
+    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const signUp = user => dispatch => (
+  SessionAPIUtil.signUp(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+);
+
+export const clearErrors = () => dispatch => (
+  dispatch(clearSessionErrors())
+);
