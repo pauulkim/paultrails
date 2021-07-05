0b582ca17fbbdb06304cda7d9f9c18275f91e16a
Paul Kim <paulwkim30@gmail.com>
2021-07-04 17:19:20 -0700

    add entities and session errors reducers

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,8 +2,8 @@ import * as SessionAPIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
-export const CLEAR_SESSION_ERRORS = "CLEAR_SESSION_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+// export const CLEAR_SESSION_ERRORS = "CLEAR_SESSION_ERRORS";
 
 const receiveCurrentUser = user => ({
   type: RECEIVE_CURRENT_USER,
@@ -14,33 +14,33 @@ const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER
 });
 
-const receiveErrors = errors => ({
-  type: RECEIVE_ERRORS,
+const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
-const clearSessionErrors = () => ({
-  type: CLEAR_SESSION_ERRORS
-});
+// const clearSessionErrors = () => ({
+//   type: CLEAR_SESSION_ERRORS
+// });
 
 export const login = user => dispatch => (
   SessionAPIUtil.login(user)
     .then(user => dispatch(receiveCurrentUser(user)))
-    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+    .fail(errors => dispatch(receiveSessionErrors(errors.responseJSON)))
 );
 
 export const logout = () => dispatch => (
   SessionAPIUtil.logout()
     .then(() => dispatch(logoutCurrentUser()))
-    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+    .fail(errors => dispatch(receiveSessionErrors(errors.responseJSON)))
 );
 
 export const signUp = user => dispatch => (
   SessionAPIUtil.signUp(user)
     .then(user => dispatch(receiveCurrentUser(user)))
-    .fail(errors => dispatch(receiveErrors(errors.responseJSON)))
+    .fail(errors => dispatch(receiveSessionErrors(errors.responseJSON)))
 );
 
-export const clearErrors = () => dispatch => (
-  dispatch(clearSessionErrors())
-);
+// export const clearErrors = () => dispatch => (
+//   dispatch(clearSessionErrors())
+// );
