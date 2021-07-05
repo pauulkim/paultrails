0cd9a4cd7aed40b7890af65716f3a36d3323c66c
Paul Kim <paulwkim30@gmail.com>
2021-07-04 22:45:10 -0700

    add testing

--- frontend/paultrails.jsx
+++ frontend/paultrails.jsx
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from "./components/root";
 
-import { login, logout } from "./actions/session_actions"
+import { login, logout, signUp } from "./actions/session_actions" // testing
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById("root");
 
+  //testing
   window.store = store;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.login = login;
   window.logout = logout;
+  window.signUp = signUp;
+  // end testing
   
   ReactDOM.render(<Root store={store} />, root)
 });
 
-
-// window.signUp = signUp;
