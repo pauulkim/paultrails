3a6442d4fb73f2192e5baa609b376b2dbc017cc6
Paul Kim <paulwkim30@gmail.com>
2021-07-05 15:18:35 -0700

    add login/signup containers and session form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router';
 import NavBarContainer from "./nav/nav_bar_container";
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
   <div>
-    <NavBarContainer />
+    <header>
+      <NavBarContainer />
+    </header>
+
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
   </div>
 );
 
--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,4 +1,86 @@
-const SessionForm
+import React from "react";
+
+// need to add errors underneath each input field
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props.formState;
+
+    this.handleSubmit = this.handleSubmit.bind(this); // REVIEW - bind vs not needing to bind
+  };
+
+  update(input) {
+    return (e) => {
+      this.setState({ [input]: e.target.value })
+    }
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.processForm(this.state);
+  };
+
+  render() {
+    const { errors, formType, formTitle, otherText, otherForm, processForm } = this.props;
+
+    return (
+      <div>
+        <h2>{formTitle}</h2>
+        <form onSubmit={this.handleSubmit}>
+            {/* only display first name and last name fields on sign up */}
+            {
+              formType === "Sign Up" ? (
+                <>
+                  <label>
+                    <input 
+                      type="text"
+                      placeholder="First name"
+                      value={this.state.firstName}
+                      onChange={this.update("firstName")}
+                      required
+                    />
+                  </label>
+                  <label>
+                    <input 
+                      type="text"
+                      placeholder="Last name"
+                      value={this.state.lastName}
+                      onChange={this.update("lastName")}
+                      required
+                    />
+                  </label>
+                </>
+              ) : ("")
+            }
+
+            <label>
+              <input 
+                type="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.update("email")}
+                required
+              />
+            </label>
+            <label>
+              <input 
+                type="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.update("password")}
+                required
+              />
+            </label>
+
+            <button>{formType}</button>
+        </form>
+        <p>{errors}</p>
+        <p>{otherText}</p>
+        <p>{otherForm}</p>
+      </div>
+    )
+  }
+}
 
 
 export default SessionForm;
