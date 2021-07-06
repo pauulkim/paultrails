6931222ef6d9c0f0307d9421361578809f001a2b
Paul Kim <paulwkim30@gmail.com>
2021-07-05 18:43:32 -0700

    add auth route

---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Redirect, withRouter } from "react-router-dom";
+
+const mSTP = state => ({
+  loggedIn: Boolean(state.session.currentUserId)
+});
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={props => loggedIn ? <Redirect to="/" /> : <Component {...props} />}
+  />
+);
+
+export const AuthRoute = withRouter(connect(mSTP, null)(Auth));
