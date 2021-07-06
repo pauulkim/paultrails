095d5baaf829f7e96210bb14901c17b8bd42dc95
Paul Kim <paulwkim30@gmail.com>
2021-07-06 12:14:11 -0700

    add park/trail model validations and associations

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,7 @@
 class Park < ApplicationRecord
+  validates :name, :description, :lat, :long, :acreage, :contact, presence: true
+  # validation unique name?
+
+  has_many :trails
+  
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,9 @@
 class Trail < ApplicationRecord
+  validates :name, :summary, :description, :difficulty, :length, :elevation_gain, :route_type, :lat, :long, :park_id, presence: true
+  validates :difficulty, inclusion: { in: ["easy", "moderate", "hard"] }
+  validates :route_type, inclusion: { in: ["Loop", "Out & back", "Point to point"] }
+  # validation unique name?
+
+  belongs_to :park
+
 end
