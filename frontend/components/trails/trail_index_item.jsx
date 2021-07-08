8a388485368c7d85e376e792b59a5606019855ec
Paul Kim <paulwkim30@gmail.com>
2021-07-08 07:46:14 -0700

    add link to trail on park page

--- frontend/components/trails/trail_index_item.jsx
+++ frontend/components/trails/trail_index_item.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const TrailIndexItem = ({ trail, parkName }) => (
   <div>
-    <h2>{trail.name}</h2>
-    <h4>{parkName}</h4>
-    <p>{trail.difficulty}</p>
-    <p>{trail.length}</p>
-    <p>{trail.summary}</p>
+    <Link to={`/trail/${trail.id}`}>
+      <h2>{trail.name}</h2>
+      <h4>{parkName}</h4>
+      <p>{trail.difficulty}</p>
+      <p>{trail.length}</p>
+      <p>{trail.summary}</p>
+    </Link>
   </div>
 );
