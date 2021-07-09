a769741a132a39d17feb2d11f3a0b90ef9af1da6
Paul Kim <paulwkim30@gmail.com>
2021-07-08 18:00:45 -0700

    migrate reviews table

---
+++ db/migrate/20210709002727_create_reviews.rb
@@ -0,0 +1,15 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.integer :rating, null: false
+      t.date :activity_date, null: false
+      t.text :review_description, null: false
+      t.integer :user_id, null: false
+      t.integer :trail_id, null: false
+
+      t.timestamps
+    end
+    add_index :reviews, :user_id
+    add_index :reviews, :trail_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_183829) do
+ActiveRecord::Schema.define(version: 2021_07_09_002727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(version: 2021_07_08_183829) do
     t.string "contact", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating", null: false
+    t.date "activity_date", null: false
+    t.text "review_description", null: false
+    t.integer "user_id", null: false
+    t.integer "trail_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trail_id"], name: "index_reviews_on_trail_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "trails", force: :cascade do |t|
