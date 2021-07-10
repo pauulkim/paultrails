925300c3436ac7c0f4bfa791ea98a340fe287050
Paul Kim <paulwkim30@gmail.com>
2021-07-09 17:53:05 -0700

    test review actions and reducer

--- frontend/paultrails.jsx
+++ frontend/paultrails.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from "./components/root";
 
-import { requestTrailReviews, makeReview, editReview, removeReview } from "./actions/review_actions"
-
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
   let store;
@@ -29,10 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
   //testing
 
   window.store = store;
-  window.requestTrailReviews = requestTrailReviews;
-  window.makeReview = makeReview;
-  window.editReview = editReview;
-  window.removeReview = removeReview;
 
   // end testing
   
