39467b78de7c7a72537d7f440fff222e448cddda
Paul Kim <paulwkim30@gmail.com>
2021-07-14 08:19:50 -0700

    add stars to parks page and add more styling

--- frontend/components/parks/park.jsx
+++ frontend/components/parks/park.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Map from "../maps/map";
 import { ParkTrailIndex } from "./park_trail_index";
+import ReactStars from "react-rating-stars-component";
 
 class Park extends React.Component {
   constructor(props) {
@@ -31,13 +32,28 @@ class Park extends React.Component {
         <img src="https://paultrails-seeds.s3.us-west-1.amazonaws.com/session_form_img.jpg"/>
         <div className="park-info">
           <h1 className="park-title">{park.name}</h1>
+          <ReactStars 
+            className="stars" 
+            count={5} 
+            value={4}
+            color="lightgray"
+            size={20}
+            edit={false}
+          /> 
           <p className="park-description">{park.description}</p>
           <Map lat={park.lat} long={park.long} type="park-map"/>
         </div>
-        <div>
+        <div className="park-minor-info">
+          <h1>Park information</h1>
           <div className="park-minor-details">
-            <p>{park.acreage}</p>
-            <p>{park.contact}</p>
+            <div className="acreage">
+              <h2>Acreage:</h2>
+              <p>{park.acreage}</p>
+            </div>
+            <div className="contact">
+              <h2>Contact</h2>
+              <p>{park.contact}</p>
+            </div>
           </div>
         </div>
         <div>
