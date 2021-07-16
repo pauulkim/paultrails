d399a8a87ed725fac915d38252590b79f2be86cf
Paul Kim <paulwkim30@gmail.com>
2021-07-15 22:48:28 -0700

    add parks buttons to home page

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -6,6 +7,10 @@ const Home = () => {
       <div className="home-page-wrapper">
         <img className="home-page-image" src={"https://paultrails-seeds.s3.us-west-1.amazonaws.com/home_page.jpg"} alt="homepage.jpg"/>
         <h1 className="home-page-greeting">Find your next adventure</h1>
+      </div>
+      <div className="temporary-home-links-container">
+        <Link to="/parks/1">Zion National Park</Link>
+        <Link to="/parks/2">Hallasan National Park, South Korea</Link>
       </div>
     </div>
   )
