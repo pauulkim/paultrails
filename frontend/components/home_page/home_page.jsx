71394ab398b8d0914ce77209f1fcfd01b2da5076
Paul Kim <paulwkim30@gmail.com>
2021-08-05 12:15:55 -0700

    rearrange home page elements and css

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -11,7 +11,11 @@ const Home = (props) => {
     <div>
       <div className="home-page-wrapper">
         <img className="home-page-image" src={"https://paultrails-seeds.s3.us-west-1.amazonaws.com/home_page.jpg"} alt="homepage.jpg"/>
-        <h1 className="home-page-greeting">Find your next adventure</h1>
+        <div className="home-page-main-content">
+          <h1>Find your next adventure</h1>
+        </div>
+        {/* <h1 className="home-page-greeting">Find your next adventure</h1>
+        <h1 className="home-page-greeting">sdf</h1> */}
       </div>
       <div className="temporary-home-links-container">
         <Link to="/parks/1">Zion National Park</Link>
