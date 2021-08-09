eeee9a0310804fca9148fe97747c1421b80b0596
Paul Kim <paulwkim30@gmail.com>
2021-08-09 16:32:06 -0700

    start search bar

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 const SearchBar = ({ search }) => {
-  console.log(search)
-
+  // console.log(search);
+  
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  
+  console.log(searchTerm);
+  
+  const changeSearchTerm = e => {
+    setSearchTerm(e.target.value);
+  }
 
   return (
-    <h1>test</h1>
+    <div>
+      <input 
+        type="text"
+        value={searchTerm}
+        onChange={changeSearchTerm}
+      />
+    </div>
   )
 };
 
