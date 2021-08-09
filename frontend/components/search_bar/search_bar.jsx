c1978e5e840c6b2a64755c4dbd29b15f42d3aa72
Paul Kim <paulwkim30@gmail.com>
2021-08-09 16:54:47 -0700

    continue work on search bar

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from "react";
 
 const SearchBar = ({ search }) => {
-  // console.log(search);
+  console.log(search);
   
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   
   console.log(searchTerm);
-  
+
   const changeSearchTerm = e => {
     setSearchTerm(e.target.value);
-  }
+  };
+
+  useEffect( () => {
+    // need to fix this logic
+    if (search.isArray) {
+      const results = search.filter( item => 
+        item.name.toLowerCase().includes(searchTerm)
+      );
+  
+      setSearchResults(results);
+    }
+
+  }, [searchTerm])
 
   return (
     <div>
@@ -19,6 +31,13 @@ const SearchBar = ({ search }) => {
         value={searchTerm}
         onChange={changeSearchTerm}
       />
+      <ul>
+        { 
+          searchResults.map( res => 
+            <li>{res.name}</li>
+          )
+        }
+      </ul>
     </div>
   )
 };
